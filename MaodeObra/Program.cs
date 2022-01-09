6c3bcf43a6977acb08d58d03e8ea2cfed158b647
Kle3er Alves <klebergithub@gmail.com>
2022-01-08 21:04:32 -0300

    PROJETO INICIADO - CALCULAR SALARIO DE MAO DE OBRA

---
+++ MaodeObra/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MaodeObra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Vai Corinthians");
+        }
+    }
+}
